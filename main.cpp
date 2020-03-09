6af252e1a748f523c8db1bab325bb6063c6a6bbe
Joona Prehti <joona.prehti@gmail.com>
2020-03-09 11:51:23 +0200

    Cleared out the main

--- main.cpp
+++ main.cpp
@@ -7,26 +7,19 @@ using namespace std;
 
 int main()
 {
-	cout << "Main alkaa!!!" << endl;
-	KoiraMiddleClass koira;
-	koira.aantele();
-
-	PuudeliSubClass puudeli;
-	puudeli.aantele();
-
 	// Polyformismi toiminnassa, puudeli on Koira luokan
 	// olion paikalla, joten sille kutsutaan luokan Koira
 	// aantele metodia
 	cout << "Polyformismi-------------------------------------------" << endl;
+	PuudeliSubClass puudeli;
 	KoiraMiddleClass koiraOlio = puudeli;
 	koiraOlio.aantele();
 
-	cout << "Pointer testailuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu!!!" << endl;
+	cout << "Pointer*****************************************************" << endl;
 	cout << "Polyformismi-------------------------------------------" << endl;
 	KoiraMiddleClass* koiraPtr = new PuudeliSubClass();
 	koiraPtr->aantele();
 
 
-	cout << "Main LOPPUU!!!" << endl;
 	return 0;
 }
